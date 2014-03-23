5b1520a567eccb5bf4b7ad810c93f16edfe53bc3
Aurelien RASTEL <aurelien.rastel@gmail.com>
2014-03-23 19:57:57 +0100

    Commit pour le gitignore, c'est mieux?

--- 4A-POO-TP/src/main/java/fr/esiea/poo/User.java
+++ 4A-POO-TP/src/main/java/fr/esiea/poo/User.java
@@ -2,26 +2,26 @@ package fr.esiea.poo;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import fr.esiea.poo.Alerte.TypeAlerte;
 import fr.esiea.poo.exception.ForbiddenBidOperation;
+import fr.esiea.poo.exception.InsuffisantOfferPrice;
 
-public class User implements Acheteur, Vendeur, ObservateurEncheres
-{
+public class User implements Acheteur, Vendeur, ObservateurEncheres {
 	private ArrayList<Enchere> userEnchere = new ArrayList<Enchere>();
 	private ArrayList<Offre> userOffres = new ArrayList<>();
 	private String login, nom, prenom;
 	private SalleEnchere salesHouse = SalleEnchere.getInstance();
 
-	public User(String login, String nom, String prenom)
-	{
+	public User(String login, String nom, String prenom) {
 		this.login = login;
 		this.nom = nom;
 		this.prenom = prenom;
 	}
 
 	@Override
-	public Enchere creerEnchere(Produit obj, Date dateLimite)
-	{
+	public Enchere creerEnchere(Produit obj, Date dateLimite) {
 		Enchere enchere = new Enchere(obj, dateLimite);
 		userEnchere.add(enchere);
 		salesHouse.creerEnchere(enchere);
@@ -30,8 +30,8 @@ public class User implements Acheteur, Vendeur, ObservateurEncheres
 	}
 
 	@Override
-	public Enchere creerEncherePrixMin(Produit obj, Date dateLimite, double prixMin)
-	{
+	public Enchere creerEncherePrixMin(Produit obj, Date dateLimite,
+			double prixMin) {
 		Enchere enchere = new Enchere(obj, dateLimite, prixMin, 0);
 		userEnchere.add(enchere);
 		salesHouse.creerEnchere(enchere);
@@ -40,8 +40,8 @@ public class User implements Acheteur, Vendeur, ObservateurEncheres
 	}
 
 	@Override
-	public Enchere creerEncherePrixReserve(Produit obj, Date dateLimite, double prixReserve)
-	{
+	public Enchere creerEncherePrixReserve(Produit obj, Date dateLimite,
+			double prixReserve) {
 		Enchere enchere = new Enchere(obj, dateLimite, 0, prixReserve);
 		userEnchere.add(enchere);
 		salesHouse.creerEnchere(enchere);
@@ -50,8 +50,8 @@ public class User implements Acheteur, Vendeur, ObservateurEncheres
 	}
 
 	@Override
-	public Enchere creerEnchere(Produit obj, Date dateLimite, double prixMin, double prixReserve)
-	{
+	public Enchere creerEnchere(Produit obj, Date dateLimite, double prixMin,
+			double prixReserve) {
 		Enchere enchere = new Enchere(obj, dateLimite, prixMin, prixReserve);
 		userEnchere.add(enchere);
 		salesHouse.creerEnchere(enchere);
@@ -60,62 +60,103 @@ public class User implements Acheteur, Vendeur, ObservateurEncheres
 	}
 
 	@Override
-	public Offre emettreOffre(Enchere ench, double prix) throws ForbiddenBidOperation
-	{
-		if (this.userEnchere.contains(ench))
-		{
-			throw new ForbiddenBidOperation("Vous ne pouvez pas enchérir sur une enchere qui vous appartient!");
+	public Offre emettreOffre(Enchere ench, double prix)
+			throws ForbiddenBidOperation {
+		if (this.userEnchere.contains(ench)) {
+			throw new ForbiddenBidOperation(
+					"Vous ne pouvez pas enchérir sur une enchere qui vous appartient!");
 		}
-		if (prix < ench.getPrixMin())
-		{
-			throw new ForbiddenBidOperation("Le prix de l'offre est inférieur au prix minimum de l'enchère!");
+		if (prix < ench.getPrixMin()) {
+			throw new ForbiddenBidOperation(
+					"Le prix de l'offre est inférieur au prix minimum de l'enchère!");
 		}
 		Offre offre = new Offre(this, prix);
 		userOffres.add(offre);
-		ench.addOffre(offre);
+		try {
+			ench.addOffre(offre);
+		} catch (InsuffisantOfferPrice e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		return offre;
 	}
 
 	@Override
-	public void publierEnchere(Enchere ench) throws ForbiddenBidOperation
-	{
-		if (!userEnchere.contains(ench))
-		{
-			throw new ForbiddenBidOperation("Error updating Bid. Probably not your bid");
-		} else
-		{
+	public void publierEnchere(Enchere ench) throws ForbiddenBidOperation {
+		if (!userEnchere.contains(ench)) {
+			throw new ForbiddenBidOperation(
+					"Error updating Bid. Probably not your bid");
+		} else {
 			ench.setEtat(Etat.PUBLIEE);
 			salesHouse.publierEnchere(ench);
 		}
 	}
 
 	@Override
-	public void annulerEnchere(Enchere ench) throws ForbiddenBidOperation 
-	{
-		if (!userEnchere.contains(ench))
-		{
-			throw new ForbiddenBidOperation("Error updating Bid. Probably not your bid.");
+	public void annulerEnchere(Enchere ench) throws ForbiddenBidOperation {
+		if (!userEnchere.contains(ench)) {
+			throw new ForbiddenBidOperation(
+					"Error updating Bid. Probably not your bid.");
 		}
-		if (ench.hasOffers())
-		{
-			throw new ForbiddenBidOperation("Impossible d'annuler cette enchère. Le prix de réserve a déjà été atteint.");
+		if (ench.hasOffers()) {
+			throw new ForbiddenBidOperation(
+					"Impossible d'annuler cette enchère. Le prix de réserve a déjà été atteint.");
 		}
 		ench.setEtat(Etat.ANNULEE);
 
 		salesHouse.annulerEnchere(ench);
 	}
 
+	/**
+	 * Méthode d'abonnement de l'User à des alertes.<br>
+	 * Il suffit de passer une liste contenant les types des alertes que l'user
+	 * souhaite recevoir, ainsi que l'enchere à laquelle il souhaite s'abonner.
+	 * 
+	 */
+	public void abonnementAlertesEnchere(Enchere _enchere, List<TypeAlerte> _listeAlertesDesirees) {
+		if (_listeAlertesDesirees.contains(TypeAlerte.ANNULATION)) {
+			_enchere.attacherObsAnnule(this);
+		}
+		if (_listeAlertesDesirees.contains(TypeAlerte.FIN_ENCHERE)) {
+			_enchere.attacherObsFinEnchere(this);
+		}
+		if (_listeAlertesDesirees.contains(TypeAlerte.RESERVE_ATTEINTE)) {
+			_enchere.attacherObsReserveAtteinte(this);
+		}
+		if (_listeAlertesDesirees.contains(TypeAlerte.SURENCHERE)) {
+			_enchere.attacherObsNvlOffre(this);
+		}
+	}
+	
+	/**
+	 * Méthode de désabonnement de l'User à des alertes.<br>
+	 * Il suffit de passer une liste contenant les types des alertes que l'user
+	 * souhaite annuler.
+	 */
+	public void desabonnementAlertesEnchere(Enchere _enchere, List<TypeAlerte> _listeAlertesASupprimer) {
+		if (_listeAlertesASupprimer.contains(TypeAlerte.ANNULATION)) {
+			_enchere.attacherObsAnnule(this);
+		}
+		if (_listeAlertesASupprimer.contains(TypeAlerte.FIN_ENCHERE)) {
+			_enchere.attacherObsFinEnchere(this);
+		}
+		if (_listeAlertesASupprimer.contains(TypeAlerte.RESERVE_ATTEINTE)) {
+			_enchere.attacherObsReserveAtteinte(this);
+		}
+		if (_listeAlertesASupprimer.contains(TypeAlerte.SURENCHERE)) {
+			_enchere.attacherObsNvlOffre(this);
+		}
+	}
+
 	@Override
-	public void receptAlerte(Alerte a)
-	{
+	public void receptAlerte(Alerte a) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public boolean isPrixReserveAtteint(Enchere ench)
-	{
+	public boolean isPrixReserveAtteint(Enchere ench) {
 		return ench.isPrixReserveAtteint();
 	}
 }
